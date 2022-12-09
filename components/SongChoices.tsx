109518f3c3a9b29510ed36c5f7f513173ace1282
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-12-09 14:13:08 -0500

    added firstLine to songchoices

--- components/SongChoices.tsx
+++ components/SongChoices.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
 import {Picker} from '@react-native-picker/picker';
+import FirstLine from './FirstLine';
 
 export const ArianaSongs = () => {
+    const [firstLine, setFirstLine] = useState('Choose an Artist to display a first line')
     const [song, Setsong] = useState('');
     return(
         <View>
@@ -16,14 +18,27 @@ export const ArianaSongs = () => {
             <Picker.Item label="7 Rings" value="seven" />
             <Picker.Item label="NASA" value="nasa" />
          </Picker>
+         <FirstLine text={firstLine}/>
         </View>
     )
 }
 
 export const TaylorSongs = () => {
+    const [firstLine, setFirstLine] = useState('Choose an Artist to display a first line')
+    const [song, Setsong] = useState('');
     return(
         <View>
-            
+         <Picker
+            selectedValue={song}
+            onValueChange={(itemValue, itemIndex) =>
+                Setsong(itemValue)
+            }>
+            <Picker.Item label="Pick A Song:" value="default"  />
+            <Picker.Item label="Love Story" value="love" />
+            <Picker.Item label="Bad Blood<" value="bad" />
+            <Picker.Item label="Enchanted" value="enchanted" />
+         </Picker>
+         <FirstLine text={firstLine}/>
         </View>
     )
 }
