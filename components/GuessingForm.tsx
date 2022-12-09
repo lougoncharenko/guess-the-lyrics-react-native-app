9b91ddca61094c00e0561d285d2ec1128b8e914e
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-12-09 15:01:57 -0500

    added styling to form and centered it

--- components/GuessingForm.tsx
+++ components/GuessingForm.tsx
@@ -12,27 +12,41 @@ const GuessingForm = (props:Props) => {
     return null;
   }
   return (
-    <View>
-        <Text> Guess the Next Line: </Text>
-        <TextInput
-          style={styles.input}
-          onChangeText={setSecondGuess}
-          value={secondGuess} 
-          placeholder = 'Enter your guess here'
-        />
-        <Pressable>
-            <Text>Submit</Text>
-        </Pressable>
+    <View style={styles.container}>
+        <View style={styles.fieldset}>
+            <Text> Guess the Next Line: </Text>
+            <TextInput
+            style={styles.input}
+            onChangeText={setSecondGuess}
+            value={secondGuess} 
+            placeholder = 'Enter your guess here'
+            />
+            <Pressable>
+                <Text>Submit</Text>
+            </Pressable>
+        </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        paddingBottom: 80,
+    },
     input: {
       height: 40,
       margin: 12,
       borderWidth: 1,
       padding: 10,
+    },
+    fieldset: {
+        padding: 10,
+        borderColor: 'black',
+        borderWidth: 5,
+        borderStyle: 'solid',
+        justifyContent: 'center',
+        width: '80%',
     },
   });
 
