874c72d2d913609b5666497d1a0e328ed62c357c
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-12-09 13:51:44 -0500

    imported FirstLine components and set prop to a useState

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,10 +2,12 @@ import React, {useState} from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 import { ariana, taylor } from './ArtistInformation';
+import FirstLine from './FirstLine';
 
 export const Header = () => {
     const [artistImage, setImage] = useState('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs5snERfT36_-ejP2Knz3U1AeYIavkIqG3mSb0paHULk1sLAn62DWcBr0tCIsh0D4UQmM&usqp=CAU')
     const [artist, setArtist] = useState<string>('')
+    const [firstLine, setFirstLine] = useState('Choose an Artist to display a first line')
     const changeImage = (itemValue:string) => {
         setArtist(itemValue)
         if (itemValue ==='ariana'){
@@ -37,6 +39,8 @@ export const Header = () => {
             <Picker.Item label="Ariana Grande" value="ariana" />
             <Picker.Item label="Taylor Swift" value="taylor" />
         </Picker>
+            <FirstLine text={firstLine}/>
+
       </View>
     )
   }
