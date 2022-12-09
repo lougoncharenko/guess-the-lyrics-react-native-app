a7964f4d8d3f0f9185649cf507af452353363478
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-12-09 13:19:03 -0500

    made a section for artist information

---
+++ components/ArtistInformation.tsx
@@ -0,0 +1,43 @@
+interface SongInformation {
+    firstLyrics: Array<string>,
+    secondLyrics: Array<string>,
+    images: Array<string>,
+  };
+  
+export const ariana: SongInformation = {
+    firstLyrics: [
+      "I've been here all night (Ariana)",
+      "Yeah, breakfast at Tiffany's and bottles of bubbles",
+      "This is one small step for woman"
+    ],
+    secondLyrics: [
+      "I've been here all day (Nicki Minaj)",
+      "Girls with tattoos who like getting in trouble",
+      "One giant leap for woman-kind"
+    ],
+    images: [
+      "https://media.giphy.com/media/upHCge5MbrLSo/giphy.gif",
+      "https://m.media-amazon.com/images/M/MV5BZWFhZWQzZTEtOGYyYy00NDY4LWI1NDktNjVlMzM4NWYxYmY0XkEyXkFqcGdeQXVyNzE5MjU4MDM@._V1_.jpg",
+      "https://m.media-amazon.com/images/M/MV5BYTQ2MTdmOTQtNzY0NS00MDQ1LWEwOWItNDczNzUxMDdlOTYzXkEyXkFqcGdeQXVyMTI1Mzg0ODA5._V1_.jpg",
+      "https://i.pinimg.com/originals/d8/bf/4a/d8bf4aba157a634ce030c5aa818f41d9.png"
+    ]
+  };
+  
+export const taylor: SongInformation = {
+    firstLyrics:[
+      "We were both young when I first saw you",
+      "Cause baby, now we got bad blood",
+      "There I was again tonight forcing laughter, faking smiles"
+    ],
+    secondLyrics: [
+      "I close my eyes and the flashback starts",
+      "You know it used to be mad love",
+      "Same old tired, lonely place"
+    ],
+    images: [
+      "https://media.tenor.com/Tnon_GhnnKUAAAAM/sassy-pretty.gif",
+      "https://upload.wikimedia.org/wikipedia/en/0/01/Taylor_Swift_-_Love_Story.png",
+      "https://upload.wikimedia.org/wikipedia/en/9/9b/Taylor_Swift_Feat._Kendrick_Lamar_-_Bad_Blood_%28Official_Single_Cover%29.png",
+      "https://img.buzzfeed.com/buzzfeed-static/static/2018-04/16/10/asset/buzzfeed-prod-web-04/sub-buzz-16652-1523887341-1.jpg?output-quality=auto&output-format=auto&downsize=640:*"
+    ]
+  };
