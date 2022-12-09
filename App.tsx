e4cb07361e4baa2a8439ca6950e9505cd22e5c7f
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-12-09 13:17:05 -0500

    saving progress

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Header from './components/Header';
+import {Header} from './components/Header';
 
 export default function App() {
   return (
--- components/Header.tsx
+++ components/Header.tsx
@@ -2,37 +2,47 @@ import React, {useState} from 'react';
 import { View, Text, Image, StyleSheet, } from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 
-const Header = () => {
-  return (
+const ArtistChoice = () => {
+    const [image, setImage] = useState('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs5snERfT36_-ejP2Knz3U1AeYIavkIqG3mSb0paHULk1sLAn62DWcBr0tCIsh0D4UQmM&usqp=CAU')
+    const [artist, setArtist] = useState<string>('')
+    return (
     <View style={styles.container}>
-        <Text style = {styles.heading}> Guess The Lyrics</Text>
-        <Image 
-        style = {styles.image}
-        source = {{
-            uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs5snERfT36_-ejP2Knz3U1AeYIavkIqG3mSb0paHULk1sLAn62DWcBr0tCIsh0D4UQmM&usqp=CAU'
-        }}
-        />
+
     </View>
   );
 }
 
-export const ArtistChoice = () => {
+export const Header = () => {
+    const [image, setImage] = useState<any>('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs5snERfT36_-ejP2Knz3U1AeYIavkIqG3mSb0paHULk1sLAn62DWcBr0tCIsh0D4UQmM&usqp=CAU')
     const [artist, setArtist] = useState<string>('')
+    if (artist ==='ariana'){
+        alert('ariana')
+    } else if (artist === 'taylor'){
+        alert('taylor')
+    } else {
+        console.log('hello')
+    }
     return (
       <View>
-          <Text style= {styles.artistText}>Select an Artist:</Text>
+          <View style= {styles.container}>
+          <Text style = {styles.heading}> Guess The Lyrics</Text>
+             <Image 
+                style = {styles.image}
+                source = { {image}
+                }
+             /> 
+          </View>
+        
          <Picker
             selectedValue={artist}
             onValueChange={(itemValue, itemIndex) =>
             setArtist(itemValue)
             }>
+            <Picker.Item label="Pick An Artist:" value="default" />
             <Picker.Item label="Ariana Grande" value="ariana" />
             <Picker.Item label="Taylor Swift" value="taylor" />
         </Picker>
-     
       </View>
-    
-  
     )
   }
 
@@ -62,4 +72,3 @@ const styles = StyleSheet.create( {
     }
 })
 
-export default Header;
