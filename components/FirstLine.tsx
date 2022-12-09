19de3ab4a630c3df403f1ffcddba3aa94423eeff
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-12-09 13:51:23 -0500

    made box centered and text flexWrap

--- components/FirstLine.tsx
+++ components/FirstLine.tsx
@@ -7,23 +7,34 @@ interface Props {
 
 const FirstLine = (props:Props) => {
   return (
-      <View style= {styles.fieldset}> 
-        <Text>
-            {props.text}
-        </Text> 
+      <View style={styles.container}>
+        <View style= {styles.fieldset}> 
+            <Text style={styles.firstLine}>
+                {props.text}
+            </Text> 
+        </View>
       </View>
+      
     
   );
 }
 
 const styles= StyleSheet.create({
+    container: {
+        alignItems: 'center',
+    },
     fieldset: {
         padding: 10,
         borderColor: 'black',
         borderWidth: 5,
-        borderStyle: 'solid'
+        borderStyle: 'solid',
+        justifyContent: 'center',
+        width: '80%'
+    },
+    firstLine: {
+        flexDirection: 'row',
+        flexWrap: 'wrap',
     }
-
 })
 
 export default FirstLine;
