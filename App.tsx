a0d472c4e8b42524e89225a767f71898a623ec0f
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-12-09 14:21:20 -0500

    changed view to scroll view

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet } from 'react-native';
 import {Header} from './components/Header';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Header />
       <StatusBar style="auto" />
-    </View>
+    </ScrollView>
   );
 }
 
