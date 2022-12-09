d26117405bd9155f96968c855009ef3772038ab4
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-12-09 14:08:57 -0500

    made ariana section

--- components/SongChoices.tsx
+++ components/SongChoices.tsx
@@ -1,6 +1,29 @@
+import { useState } from 'react';
 import { View, Text, Image, StyleSheet } from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 
-export const ArianaSongs = () => {}
+export const ArianaSongs = () => {
+    const [song, Setsong] = useState('');
+    return(
+        <View>
+         <Picker
+            selectedValue={song}
+            onValueChange={(itemValue, itemIndex) =>
+                Setsong(itemValue)
+            }>
+            <Picker.Item label="Pick A Song:" value="default"  />
+            <Picker.Item label="Side to Side" value="side" />
+            <Picker.Item label="7 Rings" value="seven" />
+            <Picker.Item label="NASA" value="nasa" />
+         </Picker>
+        </View>
+    )
+}
 
-export const TaylorSongs = () => {}
+export const TaylorSongs = () => {
+    return(
+        <View>
+            
+        </View>
+    )
+}
