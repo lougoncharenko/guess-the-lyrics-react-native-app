2028a4c3964ebc3494c51d99ae4d5bab52854124
Louisa Goncharenko <louisa.goncharenko@shopify.com>
2022-12-12 09:18:34 -0500

    added styling

--- components/Results.tsx
+++ components/Results.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 
 interface Props {
     results: string
@@ -7,10 +7,40 @@ interface Props {
 
 const Results = (props:Props) => {
   return (
-    <View>
-        <Text> {props.results} </Text>
+    <View style={styles.container}>
+    <View style= {styles.fieldset}> 
+        <Text style  = {styles.heading}>Results:</Text>
+        <Text style={styles.results}>
+            {props.results}
+        </Text> 
     </View>
-  )
+  </View>
+  
+
+);
 }
+
+const styles= StyleSheet.create({
+container: {
+    alignItems: 'center',
+    paddingBottom: 80,
+},
+fieldset: {
+    padding: 10,
+    borderColor: 'black',
+    borderWidth: 5,
+    borderStyle: 'solid',
+    justifyContent: 'center',
+    width: '80%',
+},
+heading: {
+    textAlign: 'center',
+    fontSize: 25,
+},
+results: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+}
+})
 
 export default Results
