77913d97e79983cba03dc2f919f5eff69b084cba
Louisa Goncharenko <louisa.goncharenko@shopify.com>
2022-12-12 09:12:47 -0500

    initializing

---
+++ components/Results.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Results = () => {
+  return (
+    <View>
+        <Text></Text>
+    </View>
+  )
+}
+
+export default Results
